5ff1cbe684f8f14d7ee1a61012a9f11f0439eaca
Denis Kalkopf <deniskalkopf@gmail.com>
2023-09-06 12:47:53 +0500

    will add sending an article to the backend

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { useNavigate, Navigate } from 'react-router-dom';
 import { selectIsAuth } from '../../redux/slices/auth';
+import axios from '../../axios';
 
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
@@ -12,19 +13,49 @@ import 'easymde/dist/easymde.min.css';
 import styles from './AddPost.module.scss';
 
 export const AddPost = () => {
-  const imageUrl = '';
+  const navigate = useNavigate();
   const isAuth = useSelector(selectIsAuth);
-  const [value, setValue] = React.useState('');
+  const [isLoading, setLoading] = React.useState(false);
+  const [text, setText] = React.useState('');
   const [title, setTitle] = React.useState('');
   const [tags, setTags] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
+  const inputFileRef = React.useRef(null);
 
-  const handleChangeFile = () => {};
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert('Ошибка при загрузке файла');
+    }
+  };
 
-  const onClickRemoveImage = () => {};
+  const onClickRemoveImage = () => {
+    setImageUrl('');
+  };
 
   const onChange = React.useCallback((value) => {
-    setValue(value);
+    setText(value);
   }, []);
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
+
+      const fields = { title, imageUrl, tags: tags.split(','), text };
+      const { data } = await axios.post('/posts', fields);
+
+      const id = data._id;
+      navigate(`/posts/${id}`);
+    } catch (err) {
+      console.warn(alert('Ошибка при создании статьи'));
+    }
+  };
 
   const options = React.useMemo(
     () => ({
@@ -45,21 +76,19 @@ export const AddPost = () => {
     return <Navigate to='/' />;
   }
 
-  console.log({ title, tags, value });
-
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant='outlined' size='large'>
+      <Button onClick={() => inputFileRef.current.click()} variant='outlined' size='large'>
         Загрузить превью
       </Button>
-      <input type='file' onChange={handleChangeFile} hidden />
+      <input ref={inputFileRef} type='file' onChange={handleChangeFile} hidden />
       {imageUrl && (
-        <Button variant='contained' color='error' onClick={onClickRemoveImage}>
-          Удалить
-        </Button>
-      )}
-      {imageUrl && (
-        <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt='Uploaded' />
+        <>
+          <Button variant='contained' color='error' onClick={onClickRemoveImage}>
+            Удалить
+          </Button>
+          <img className={styles.image} src={`http://localhost:4444${imageUrl}`} alt='Uploaded' />
+        </>
       )}
       <br />
       <br />
@@ -79,9 +108,9 @@ export const AddPost = () => {
         onChange={(e) => setTags(e.target.value)}
         fullWidth
       />
-      <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
+      <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
-        <Button size='large' variant='contained'>
+        <Button onClick={onSubmit} size='large' variant='contained'>
           Опубликовать
         </Button>
         <a href='/'>
--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import axios from '../axios';
+import ReactMarkdown from 'react-markdown';
 
 import { Post } from '../components/Post';
 import { Index } from '../components/AddComment';
@@ -34,7 +35,7 @@ export const FullPost = () => {
       <Post
         id={data._id}
         title={data.title}
-        imageUrl={data.imageUrl}
+        imageUrl={`http://localhost:4444${data.imageUrl}`}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
@@ -42,7 +43,7 @@ export const FullPost = () => {
         tags={data.tags}
         isFullPost
       >
-        <p>{data.text}</p>
+        <ReactMarkdown children={data.text} />
       </Post>
       <CommentsBlock
         items={[
